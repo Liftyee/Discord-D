f52225ea9f32a60ed6c541ab6fa3d22f06577525
Spamri123 <65345467+Spamri123@users.noreply.github.com>
2020-05-14 19:35:06 +0100

    Update Archie code notoken.py

--- Archie code notoken.py
+++ Archie code notoken.py
@@ -3,7 +3,7 @@
 #
 #  discord bot.py
 #  
-#  Copyright 2020 Victor <victor@victor-linux>
+#  Copyright 2020 Archie
 #  
 #  This program is free software; you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
